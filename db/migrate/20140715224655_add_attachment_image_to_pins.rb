e1221cdbc9f6857e3bb26a1e9a45162027c094da
Tyler Browder <tbrowder1@gmail.com>
2014-07-29 10:13:54 -0500

    Change Image Migration

--- db/migrate/20140715224655_add_attachment_image_to_pins.rb
+++ db/migrate/20140715224655_add_attachment_image_to_pins.rb
@@ -1,9 +1,8 @@
 class AddAttachmentImageToPins < ActiveRecord::Migration
   def self.up
-    change_table :pins do |t|
-      t.attachment :image
+    add_attachment :pins, :image
     end
-  end
+  
 
   def self.down
     remove_attachment :pins, :image
